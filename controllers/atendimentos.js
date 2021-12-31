a5204ff0f5bca443d6396d8f99aacb869124a6c1
adriano25klein@gmail.com <you@example.com>
2021-12-31 09:15:44 -0300

    Importei o Atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,10 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
   app.get("/atendimento", (req, res) => res.send('Rota atendimentos'));
 
   app.post("/atendimentos", (req, res) => {
-    console.log("atendimento enviado")
-    console.log(req.body)
+    
     res.send("Post atendimento")
   })
 }
