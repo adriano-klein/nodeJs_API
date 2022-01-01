999206579cb1836a9a38d2cbcdbb4e06b12f62e7
adriano25klein@gmail.com <you@example.com>
2022-01-01 17:54:52 -0300

    Aplicando validações de entrada

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,22 +1,46 @@
-const moment = require('moment')
-const conexao = require('../infra/conexao')
+const moment = require("moment");
+const conexao = require("../infra/conexao");
 
 class Atendimentos {
   adicionar(atendimento, res) {
-    const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
-    const data = moment(atendimento.data,'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+    const dataCriacao = moment().format("YYYY-MM-DD HH:MM:SS");
+    const data = moment(atendimento.data, "DD/MM/YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
 
-    const atendimentoDatado = { ...atendimento, dataCriacao, data }
-    const sql = 'INSERT INTO Atendimentos SET ?'
+    const dataValida = moment(data).isSameOrAfter(dataCriacao);
+    const clienteValido = atendimento.cliente.length >= 5;
 
-    conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-      if (erro) {
-        res.status(400).json(erro)
-      } else {
-        res.status(201).json(resultados)
-      }
-    })
+    const validacoes = [
+      {
+        nome: "data",
+        valido: dataValida,
+        mensagem: "A data deve ser maior ou igual a data atual",
+      },
+      {
+        nome: "cliente",
+        valido: clienteValido,
+        mensagem: "O nome do cliente deve ter pelo menos 5 caracteres",
+      },
+    ];
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const atendimentoDatado = { ...atendimento, dataCriacao, data };
+      const sql = "INSERT INTO Atendimentos SET ?";
+
+      conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(resultados);
+        }
+      });
+    }
   }
 }
 
-module.exports = new Atendimentos
+module.exports = new Atendimentos();
