0248aafd252f217175aa6d38cd5ebd3f51b8c9b1
adriano25klein@gmail.com <you@example.com>
2022-01-03 11:00:25 -0300

    04 Lidando com erros no tipo de imagens

--- arquivos/uploadArquivos.js
+++ arquivos/uploadArquivos.js
@@ -1,9 +1,18 @@
 const fs = require("fs");
+const path = require("path");
 
-module.exports = (path, fileName, callBackImageCreated) => {
-  const newPath = `./arquivos/assets/images/${fileName}`;
+module.exports = (filePath, fileName, callBackImageCreated) => {
+  const type = path.extname(filePath);
+  const newPath = `./arquivos/assets/images/${fileName}${type}`;
+  const validTypes = ["jpg", "jpeg", "png"];
+  const isValid = validTypes.indexOf(type.substring(1)) !== -1;
 
-  fs.createReadStream(path)
-    .pipe(fs.createWriteStream(newPath))
-    .on("finish", () => callBackImageCreated(newPath));
+  if (isValid) {
+    fs.createReadStream(filePath)
+      .pipe(fs.createWriteStream(newPath))
+      .on("finish", () => callBackImageCreated(false, newPath));
+  } else {
+    const erro = "Tipo de imagem inválida";
+    callBackImageCreated(erro);
+  }
 };
--- models/pets.js
+++ models/pets.js
@@ -4,17 +4,20 @@ const fileUpload = require("../arquivos/uploadArquivos");
 class Pets {
   adiciona(pet, res) {
     const query = "INSERT INTO Pets SET ?";
-    fileUpload(pet.imagem, pet.nome, (newPath) => {
-      const newPet = { nome: pet.nome, imagem: newPath };
-
-      conexao.query(query, newPet, (erro) => {
-        if (erro) {
-          console.log(erro);
-          res.status(400).json(erro);
-        } else {
-          res.status(200).json(newPet);
-        }
-      });
+    fileUpload(pet.imagem, pet.nome, (erro, newPath) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        const newPet = { nome: pet.nome, imagem: newPath };
+        conexao.query(query, newPet, (erro) => {
+          if (erro) {
+            console.log(erro);
+            res.status(400).json(erro);
+          } else {
+            res.status(200).json(newPet);
+          }
+        });
+      }
     });
   }
 }
