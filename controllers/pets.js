af16cfc5a67d5730196c4234af7470e00931eeef
adriano25klein@gmail.com <you@example.com>
2022-01-03 09:43:08 -0300

    Rota para criar pet

---
+++ controllers/pets.js
@@ -0,0 +1,8 @@
+const Pets = require("../models/pets");
+
+module.exports = (app) => {
+  app.post("/pets", (req, res) => {
+    const pet = req.body;
+    Pets.adiciona(pet, res);
+  });
+};
--- infra/tables.js
+++ infra/tables.js
@@ -2,6 +2,7 @@ class Tables {
   init(conexao) {
     this.conexao = conexao;
     this.createAttendances();
+    this.createPets();
   }
 
   createAttendances() {
@@ -13,6 +14,19 @@ class Tables {
         console.log(erro);
       } else {
         console.log("Tabela de Atendimentos criada com sucesso");
+      }
+    });
+  }
+
+  createPets() {
+    const query =
+      "CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT, nome varchar(50), imagem varchar(200), PRIMARY KEY (id))";
+
+    this.conexao.query(query, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela Pets criada com sucesso");
       }
     });
   }
